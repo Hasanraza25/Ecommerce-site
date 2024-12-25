1a139d0c4e5593693e78f05df9e55c8ca0ec0ef2
Hasanraza25 <hasansaeed2526@gmail.com>
2024-12-24 21:09:56 -0600

    add to cart functionality

--- src/app/context/CartContext.jsx
+++ src/app/context/CartContext.jsx
@@ -1,55 +1,53 @@
 "use client";
 import React, { createContext, useState, useContext, useEffect } from "react";
 
+// Create a context for the cart
 const CartContext = createContext();
 
+// Custom hook to use the CartContext
 export const useCart = () => useContext(CartContext);
 
+// CartProvider component to provide cart context to its children
 export const CartProvider = ({ children }) => {
+  // State to store cart items
   const [cartItems, setCartItems] = useState([]);
 
-  const loadCart = async () => {
-    const response = await fetch("/api/cart");
-    const data = await response.json();
-    setCartItems(data);
-  };
-
-  const saveCart = async (items) => {
-    await fetch("/api/cart", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ cartItems: items }),
-    });
-  };
-
+  // Load cart items from localStorage when the component mounts
   useEffect(() => {
-    loadCart();
+    const storedCartItems = localStorage.getItem("cartItems");
+    if (storedCartItems) {
+      setCartItems(JSON.parse(storedCartItems));
+    }
   }, []);
 
+  // Save cart items to localStorage whenever they change
   useEffect(() => {
-    saveCart(cartItems);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
 
+  // Function to add a product to the cart
   const addToCart = (product) => {
     setCartItems((prevItems) => {
       const existingItem = prevItems.find((item) => item.id === product.id);
       if (existingItem) {
+        // If item already in cart, update quantity
         return prevItems.map((item) =>
           item.id === product.id
             ? { ...item, quantity: item.quantity + 1 }
             : item
         );
       }
+      // Add new product to cart
       return [...prevItems, { ...product, quantity: 1 }];
     });
   };
 
+  // Function to remove a product from the cart
   const removeFromCart = (id) => {
     setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
+  // Function to calculate the total price of the cart items
   const calculateTotal = () => {
     return cartItems.reduce(
       (acc, item) => acc + item.discountedPrice * item.quantity,
@@ -57,6 +55,7 @@ export const CartProvider = ({ children }) => {
     );
   };
 
+  // Function to handle quantity change of a cart item
   const handleQuantityChange = (id, change) => {
     setCartItems((prevItems) =>
       prevItems.map((item) =>
